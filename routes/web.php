f693ada6767c4bef7396901c03c7697e1a9fa721
duq <ptd.duke@gmail.com>
2018-01-18 02:28:27 +0100

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,49 @@
+<?php
+
+use App\Events\MessagePosted;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::prefix('api')->group(function() {
+    Route::resource('tasks','TaskController');
+});
+
+Route::get('test', function () {
+    return App\PrivateMessage::where('id',1)->first();
+});
+
+Route::get('/chat', function(){
+   return view('chat');
+})->middleware('auth');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/messages', function (){
+    return App\Message::with('user')->get();
+})->middleware('auth');
+
+Route::post('/messages', function (){
+    $user = Auth::user();
+    $message = $user->messages()->create([
+        'message' => request()->get('message')
+    ]);
+
+    broadcast(new MessagePosted($message, $user))->toOthers();
+
+    return ['status' => 'OK'];
+})->middleware('auth');
